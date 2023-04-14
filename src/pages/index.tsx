c44a29f5e87c71720ce4c84b6ae1ffcd7a5d2789
takumi-pro <takumidiary.0927@gmail.com>
2023-04-14 18:51:02 +0900

    feat: :sparkles: test commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 
 import styles from '@/styles/Home.module.css';
 const inter = Inter({ subsets: ['latin'] });
-
+const test: any = 'test';
 const testTest = 'test';
 console.log(testTest);
 export default function Home() {
